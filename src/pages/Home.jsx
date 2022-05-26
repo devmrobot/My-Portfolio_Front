4694edca51e77f8d31b49f9935004b5b8db61527
guiguididi <guiguididi@imac-de-guiguididi-1.home>
2022-05-26 23:25:50 +0200

    setup & home ok in progress

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../styles/home.scss";
+
+const Home = () => {
+  return (
+    <div className='main'>
+        <div className='bloc'>
+        <h1>m.robot</h1>
+        <h2>Front-end developer & digital marketer</h2>
+        <p>Guillaume PINOT - dev@mrobot.fr - 06 20 30 08 93</p>
+        </div>
+    </div>
+  )
+}
+
+export default Home
